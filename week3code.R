febc566b239185fbd15c8dc80fb5acdfb255e5fd
SankalpOjha <sojha@colgate.edu>
2025-02-12 15:13:28 -0500

    Redoing the process as the factorize tool gives a list and is not as easy to work with because it varying size of elements in the list

--- week3code.R
+++ week3code.R
@@ -1,5 +1,3 @@
-library("sfsmisc")
-
 nums <- c(6, 10, 14, 15, 21,
           22, 26, 33, 34, 35,
           38, 39, 46, 51, 55,
@@ -7,7 +5,28 @@ nums <- c(6, 10, 14, 15, 21,
           75, 77, 82, 85, 86,
           87, 91, 93, 94, 95)
 
+possible.nums <- 1:100
+first.hundred.primes <- c()
 
+#first find all the prime numbers up to 100
+for(i in 1:length(possible.nums)){
+  possible.nums.factors <- c()
+  for(j in 1:i){
+    if(i%%j == 0){ #checks for no remainder
+      possible.nums.factors <- c(possible.nums.factors, j)
+    }
+  }
+  if(length(possible.nums.factors) <= 2 
+     & !(i %in% first.hundred.primes)){ #checks possible factors to be 2
+                                        #and for the number to not be in the
+                                        #vector of the first hunderd numbers
+    first.hundred.primes <- c(first.hundred.primes, i)
+  }
+}
+
+
+
+#Old code
 factors <- factorize(nums)
 
 #found code on stack overflow
@@ -20,19 +39,19 @@ for(i in 1:length(factors)){
     
 }
 
-possible.nums <- 1:99
 
-possible.factors <- factorize(possible.nums)
 
 #found code on stack overflow
 
 possible.subs <- c()
 
 #for(i in 1:length(possible.factors)){
-i = 89
+i = 87
   if(possible.factors[[i]][2] != 1 | 
      possible.factors[[i]][3] != 1 | 
-     possible.factors[[i]][4] != 1){
+     possible.factors[[i]][4] != 1 |
+     possible.factors[[i]][3] != "NA" |
+     possible.factors[[i]][4] != "NA"){
     possible.subs <- c(possible.subs, possible.factors[i])
   }
   
